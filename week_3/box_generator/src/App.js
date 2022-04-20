7124246d32267a2533fc9c9245f7daa8d7d8c53e
Ian Page <ianpageproductions@gmail.com>
2022-04-19 20:48:46 -0700

    styles n link

--- week_3/box_generator/src/App.js
+++ week_3/box_generator/src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>Box Generator</h1>
+      <a target="_blank" href='https://www.w3schools.com/colors/colors_names.asp'> Check out the colors list! </a>
       <BoxCreator BoxArray={ BoxArray } setBoxArray={setBoxArray} />
       <BoxDisplay BoxArray={ BoxArray } />
       
