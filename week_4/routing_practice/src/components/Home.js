173c1dec321995167fe5388ae4be1bc5224025dc
Ian Page <ianpageproductions@gmail.com>
2022-04-28 14:33:03 -0700

    hghghg

--- week_4/routing_practice/src/components/Home.js
+++ week_4/routing_practice/src/components/Home.js
@@ -4,8 +4,7 @@ const Home =(props) => {
     return(
     <div>
         <h1>Welcome Home</h1>
-        <p>From here you can type in parameters in the address bar example:
-        </p>
+        <p>From here you can type in parameters in the address bar example:</p>
         <p>/word or number?/color?/backgroundcolor?</p>
         <p>/Homeboys/red/black</p>
         <p>/69/pink/black</p>
