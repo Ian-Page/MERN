87efb2f99e37a86f085ded289b218a7f771d9dba
Ian Page <ianpageproductions@gmail.com>
2022-09-03 16:36:03 -0700

    broken but does work as intended ifdouble click

--- pa_stuffs/sortvisualizer/src/SortingVisualizer/SortingVisualizer.jsx
+++ pa_stuffs/sortvisualizer/src/SortingVisualizer/SortingVisualizer.jsx
@@ -4,13 +4,13 @@ import * as sortingAlgorithims from './sortingAlgorithims'
 
 
 // Change this value for the speed of the animations.
-const ANIMATION_SPEED_MS = 10;
+const ANIMATION_SPEED_MS = .5;
 // Change this value for the number of bars (value) in the array.
-const NUMBER_OF_ARRAY_BARS = 600;
+const NUMBER_OF_ARRAY_BARS = 900;
 //this is the main color of array bars duhhhh
 const PRIMARY_COLOR ='indigo';
 // This is the color of array bars that are being compared throughout the animations.
-const SECONDARY_COLOR ='orange';
+const SECONDARY_COLOR ='lime';
 
 export default class SortingVisualizer extends React.Component{
     constructor(props){
@@ -27,8 +27,8 @@ componentDidMount(){
 
 resetArray(){
     const array = [];
-    for (let i = 0; i < 100; i++){
-        array.push(randomIntFromInterval(5, 600));
+    for (let i = 0; i < 300; i++){
+        array.push(randomIntFromInterval(0, 300));
         }
         this.setState({array});
         
@@ -37,7 +37,7 @@ mergeSort(){
     const animations = sortingAlgorithims.getmergeSortAnimations(this.state.array);
     for (let i = 0; i < animations.length; i++){
         const arrayBars = document.getElementsByClassName('array-bar');
-        const isColorChange = i % 3 !== 2;
+        const isColorChange = i % 3 !== 6;
         if (isColorChange){
             const [barOneIdx, barTwoIdx] = animations[i];
             const barOneStyle = arrayBars[barOneIdx].style;
@@ -72,23 +72,6 @@ render(){
     const {array} = this.state;
 
     return(
-
-            <div className='buttons-row'>
-            <div className='button-row'>
-            <button onClick={()=> this.resetArray()}>Generate A New Array</button>
-            </div>
-            <div className='button-row'>
-            <button onClick={()=> this.mergeSort()}>Merge Sort</button>
-            </div>
-            <div className='button-row'>
-            <button onClick={()=> this.quickSort()}>Quick Sort</button>
-            </div>
-            <div className='button-row'>
-            <button onClick={()=> this.heapSort()}>heapSort Sort</button>
-            </div>
-            <div className='button-row'>
-            <button onClick={()=> this.bubbleSort()}>Bubble Sort</button>
-            </div>
         <div>
         <div className="array-container">
         {array.map((value,idx) =>(
@@ -102,6 +85,22 @@ render(){
             
             
         ))}
+        </div>
+        <div className='buttons-row'>
+        <div className='button-row'>
+        <button onClick={()=> this.resetArray()}>Generate A New Array</button>
+        </div>
+        <div className='button-row'>
+        <button onClick={()=> this.mergeSort()}>Merge Sort</button>
+        </div>
+        <div className='button-row'>
+        <button onClick={()=> this.quickSort()}>Quick Sort</button>
+        </div>
+        <div className='button-row'>
+        <button onClick={()=> this.heapSort()}>heapSort Sort</button>
+        </div>
+        <div className='button-row'>
+        <button onClick={()=> this.bubbleSort()}>Bubble Sort</button>
         </div>
         
 
@@ -117,11 +116,10 @@ function randomIntFromInterval(min,max){
     return Math.floor (Math.random() * (max-min + 1 )+ min);
 }
 //function arraysAreEqual(arrayOne, arrayTwo){
-//    if (arrayOne.length !== arrayTwo.length) return false;
- //   for (let i =0; i < arrayOne.length; i++){
-//        if (arrayOne[i] !== arrayTwo[i]) return false;
- //   }
- //   return true;
+  // if (arrayOne.length !== arrayTwo.length) return false;
+  ///    if (arrayOne[i] !== arrayTwo[i]) return false;
+  //}
+  //return true;
 
     
 //}
