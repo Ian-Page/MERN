8607387d38ddd952442ae780dad6a83cc4a44af3
Ian Page <ianpageproductions@gmail.com>
2022-10-02 17:08:26 -0700

    prepare for chat

--- tinder-clone-app/src/components/Home.js
+++ tinder-clone-app/src/components/Home.js
@@ -4,15 +4,15 @@ import database from './firebase';
 
 function Home (){
     const[people,setPeople]= useState([
-        {
-            name:'steve jobs',
-            url: 'https://papers.co/wallpaper/papers.co-hc03-young-steve-jobs-face-1-wallpaper.jpg'
-        },
-        {
-            name:'Pamella Anderson',
-            url: 'https://images-na.ssl-images-amazon.com/images/I/41jwZM0FHlL._AC_UL600_SR600,600_.jpg'
-        }
-        
+    //   {
+    //        name:'steve jobs',
+    //        url: 'https://papers.co/wallpaper/papers.co-hc03-young-steve-jobs-face-1-wallpaper.jpg'
+    //    },
+    //    {
+    //        name:'Pamella Anderson',
+    //        url: 'https://images-na.ssl-images-amazon.com/images/I/41jwZM0FHlL._AC_UL600_SR600,600_.jpg'
+    //    }
+    //    
     ]);
     
     useEffect(()=>{
