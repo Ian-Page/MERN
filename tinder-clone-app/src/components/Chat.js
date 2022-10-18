2798aac7ee5bc3241e069d544ea15e643ea100ae
Ian Page <ianpageproductions@gmail.com>
2022-10-18 12:39:38 -0700

    stufffs

--- tinder-clone-app/src/components/Chat.js
+++ tinder-clone-app/src/components/Chat.js
@@ -3,14 +3,14 @@ function Chat (){
     return(
         <div class="Chat">
             <div className="message-box-wrapper">
-            <div className="profile-pic">
+            <div className="">
             <img src ="https://img4.hotnessrater.com/4838267/jena-frumes-bikini.jpg?w=600&h=900" className="profile-pic"  ></img>
             </div>
             <div className="message-box">STUFF YOU SAY GOES HEREEEEEEE!</div>
             </div>
             <div className="message-box-wrapper-match">
             <div className="message-box-match">STUFF they SAY GOES HEREEEEEEE!</div>
-            <div className="profile-pic">
+            <div className="">
             <img src ="https://papers.co/wallpaper/papers.co-hc03-young-steve-jobs-face-1-wallpaper.jpg" className="profile-pic"  ></img>
             </div>
             </div>
